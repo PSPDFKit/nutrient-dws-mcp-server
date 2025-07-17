5467e933cbf2a180df7f1433492e582f63e29843
nickwinder <nfxdevelopment@gmail.com>
2025-07-18 10:41:40 +1200

    Remove `tsx` dependency and update clean script and ESLint ignores

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,7 @@ export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.recommended,
   {
-    ignores: ['./dist/**', './dxt/**'],
+    ignores: ['./dist/**', './dxt_build/**', './dxt_output/**'],
   },
   {
     rules: {
